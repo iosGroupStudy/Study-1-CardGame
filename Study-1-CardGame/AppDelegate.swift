c79017e523065fd7accb4742ecd231b104971276
Dong Uk Kim <sanich8355@daum.net>
2020-05-20 20:30:48 +0900

    Initial Commit

---
+++ Study-1-CardGame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hi
+//
+//  Created by 김동욱 on 2020/05/18.
+//  Copyright © 2020 sanichdaniel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    // 개념: 앱의 시작점. 앱이 실행되기 직전에 불린다.
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        let vc = ViewController()
+        vc.view.backgroundColor = UIColor.white.withAlphaComponent(0.3)
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ Study-1-CardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Study-1-CardGame
+//
+//  Created by 김동욱 on 2020/05/20.
+//  Copyright © 2020 sanichdaniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
